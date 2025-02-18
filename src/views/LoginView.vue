ed727a12899b970bbb3f6d805ac9983e6fb15f67
Jasper Alani <jasperalani@gmail.com>
2025-02-19 04:04:44 +1300

    Initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<!--<script setup lang="ts">-->
+
+<!--</script>-->
+
+<!--<template>-->
+<!--  Welcome to {{ 'nebu' }}-->
+<!--  <div @click="signInWithGoogle">-->
+<!--    Continue with google-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
